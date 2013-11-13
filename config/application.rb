a01102ac969df76b126ce12080311c616adf14fd
javagg <lu.lee05@gmail.com>
2013-11-13 10:09:08 +0800

    assets

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,10 @@ module OpenshiftConsole
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku requires this to be false
+    config.assets.initialize_on_precompile = false
+
     config.i18n.default_locale = :"zh-CN"
   end
 end
